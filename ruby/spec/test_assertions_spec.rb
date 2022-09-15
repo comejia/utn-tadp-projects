1fe0fe1b54214168144ebcd3bf676a3485ef5c43
comejia <comejia@teco.com.ar>
2022-09-14 22:49:35 -0300

    Se agregan mas aserciones

--- ruby/spec/test_assertions_spec.rb
+++ ruby/spec/test_assertions_spec.rb
@@ -8,7 +8,23 @@ describe 'Assertions' do
     Dummy.new
   }
 
-  it 'un objeto deberia tener el metodo "deberia"' do
+  it 'un objeto entiende el mensaje "deberia"' do
     expect(una_clase.methods.include? :deberia).to be true
+  end
+
+  it 'un numero deberia ser el mismo numero' do
+    expect(7.deberia ser 7).to be true
+  end
+
+  it 'un numero es diferente a otro si no es el mismo numero' do
+    expect(7.deberia ser 8).to be false
+  end
+
+  it 'un valor verdadero deberia ser siempre verdadero' do
+    expect(true.deberia ser true).to be true
+  end
+
+  it 'un valor falso nunca puede ser verdadero' do
+    expect(false.deberia ser true).to be false
   end
 end
