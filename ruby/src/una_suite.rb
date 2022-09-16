b10092b4ad268657a31a87b2521302842f0c627f
comejia <comejia@teco.com.ar>
2022-09-16 11:45:49 -0300

    se agregan tests

--- ruby/src/una_suite.rb
+++ ruby/src/una_suite.rb
@@ -1,5 +1,4 @@
 require_relative 'persona'
-#require_relative 'tad_spec'
 
 class UnaSuite
   def testear_que_una_persona_mayor_tiene_mas_de_50
@@ -10,5 +9,17 @@ class UnaSuite
   def testear_que_una_persona_mayor_es_viejo
     p = Persona.new(80)
     p.deberia ser_viejo
+  end
+
+  def testear_que_verdadero_siempre_es_verdadero
+    true.deberia ser true
+  end
+
+  def testear_que_un_numero_es_mayor_a_otro
+    100.deberia ser mayor_a 20
+  end
+
+  def si_no_es_un_test_se_ignora
+    false.deberia ser true
   end
 end
