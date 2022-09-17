b74ea7b900b3c8fe7c1434d9b7cf161b49a968cd
comejia <comejia@teco.com.ar>
2022-09-16 22:31:33 -0300

    Modelado de condicion Tener y sus tests

--- ruby/src/suite_persona.rb
+++ ruby/src/suite_persona.rb
@@ -31,4 +31,40 @@ class SuitePersona
     p = Persona.new(18)
     p.deberia ser_viejo
   end
+
+  # Comento solo para que no explote
+  #def testear_que_una_persona_no_entiend_un_mensaje
+  #p = Persona.new(18)
+  #p.deberia ser_joven
+  #end
+
+  def testear_que_una_persona_tenga_25
+    p = Persona.new(25)
+    p.deberia tener_edad 25
+  end
+
+  def testear_que_una_persona_sin_nombre_falla
+    p = Persona.new(25)
+    p.deberia tener_nombre "pepe"
+  end
+
+  def testear_que_una_persona_sin_nombre_es_nil
+    p = Persona.new(25)
+    p.deberia tener_nombre nil
+  end
+
+  def testear_que_una_persona_tenga_edad_mayor_a_25
+    p = Persona.new(30)
+    p.deberia tener_edad mayor_a 25
+  end
+
+  def testear_que_una_persona_tenga_edad_menor_a_25
+    p = Persona.new(20)
+    p.deberia tener_edad menor_a 25
+  end
+
+  def testear_que_una_persona_tenga_edad_en_una_lista
+    p = Persona.new(20)
+    p.deberia tener_edad uno_de_estos [7, 55, true, "lista", 20]
+  end
 end
--- ruby/src/tad_spec.rb
+++ ruby/src/tad_spec.rb
@@ -46,6 +46,18 @@ module SyntaxSugar
       return proc do |x|
         x.send(mensaje.to_sym)
       end
+    elsif symbol.to_s.start_with?('tener_')
+      atributo = '@' + symbol.to_s.gsub('tener_', '')
+      return proc do |x|
+        value = x.instance_variable_get(atributo.to_sym)
+
+        if args[0].is_a? Proc
+          result = args[0].call(value)
+        else
+          result = args[0].eql? value
+        end
+        result
+      end
     end
     nil
   end
