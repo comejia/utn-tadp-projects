fda651ec3f5398a5d01f36dd2b261de769bc3580
comejia <comejia@teco.com.ar>
2022-09-27 20:47:49 -0300

    Se carga suites del contexto

--- ruby/src/tad_spec.rb
+++ ruby/src/tad_spec.rb
@@ -3,15 +3,28 @@ require_relative 'syntax_sugar'
 
 class TADSpec
 
-  def self.testear(param)
+  def self.testear(*params)
     Object.include Assertions
 
-    param.include SyntaxSugar
+    suites = get_suites *params
 
-    results = self.run_tests param
+    results = []
+    suites.each do |suite|
+      suite.include SyntaxSugar
+      results.append(run_tests suite)
+    end
 
     print_test_results results
 
+  end
+
+  def self.get_suites(*params)
+    case params.length
+    when 0
+      ObjectSpace.each_object(Class).select { |cls| is_suite?(cls) }
+    else
+      [params.first]
+    end
   end
 
   def self.run_tests(suite)
@@ -19,15 +32,23 @@ class TADSpec
     tests = self.get_suite_tests suite
     suite_obj = suite.new
 
-    tests.each do |t|
-      results << { test_name: t, status: suite_obj.send(t.to_sym) }
+    tests.each do |test|
+      results << { suite_name: suite, test_name: test, status: suite_obj.send(test.to_sym) }
     end
     results
   end
 
   def self.get_suite_tests(suite)
     methods = suite.instance_methods false
-    methods.filter { |m| m.to_s.start_with?('testear_que') }
+    methods.filter { |method| is_test?(method) }
+  end
+
+  def self.is_suite?(suite)
+    suite.instance_methods(false).any? { |method| is_test?(method) }
+  end
+
+  def self.is_test?(method)
+    method.to_s.start_with?('testear_que')
   end
 
   def self.print_test_results(results)
@@ -35,4 +56,3 @@ class TADSpec
   end
 
 end
-
