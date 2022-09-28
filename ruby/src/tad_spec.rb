35ccd950218e25f570b4553e94faa972c33c6172
comejia <comejia@teco.com.ar>
2022-09-28 00:58:53 -0300

    Se modela clases Suite y Test

--- ruby/src/tad_spec.rb
+++ ruby/src/tad_spec.rb
@@ -1,44 +1,67 @@
 require_relative 'aserciones'
 require_relative 'syntax_sugar'
+require_relative 'suite'
+require_relative 'test'
 
 class TADSpec
 
   def self.testear(*params)
+    @suites = []
     Object.include Assertions
 
-    suites = get_suites *params
+    #suites = get_suites *params
 
-    results = []
-    suites.each do |suite|
-      suite.include SyntaxSugar
-      results.append(run_tests suite)
+    get_suites *params
+
+    #results = []
+    @suites.each do |suite|
+      #suite.include SyntaxSugar
+      run_tests suite
+      #results.append(run_tests suite)
     end
 
-    print_test_results results
+    print_test_results @suites#results
 
   end
 
   def self.get_suites(*params)
     case params.length
     when 0
-      ObjectSpace.each_object(Class).select { |cls| is_suite?(cls) }
+      suites = ObjectSpace.each_object(Class).select { |cls| is_suite?(cls) }
+      suites.each do |suite|
+        new_suite = Suite.new suite
+        new_suite.add_tests(get_tests suite)
+        @suites.push(new_suite)
+      end
+    when 1
+      suite = Suite.new params.first
+      suite.add_tests(get_tests params.first)
+      @suites.push(suite)
     else
-      [params.first]
+      suite = Suite.new params.first
+      tests = params.drop(1).map do |test|
+        test.to_s.prepend("testear_que_").to_sym
+      end
+      suite.add_tests(tests)
+      @suites.push(suite)
     end
   end
 
   def self.run_tests(suite)
-    results = []
-    tests = self.get_suite_tests suite
-    suite_obj = suite.new
+    #puts suite
+    #results = []
+    #tests = self.get_tests suite
+    suite.name.include SyntaxSugar
+    suite_obj = suite.name.new
 
-    tests.each do |test|
-      results << { suite_name: suite, test_name: test, status: suite_obj.send(test.to_sym) }
+    suite.tests.each do |test|
+      #results << { suite_name: suite, test_name: test, status: suite_obj.send(test.to_sym) }
+      test.status = suite_obj.send(test.name)
     end
-    results
+    #results
   end
 
-  def self.get_suite_tests(suite)
+  def self.get_tests(suite)
     methods = suite.instance_methods false
     methods.filter { |method| is_test?(method) }
   end
@@ -52,7 +75,9 @@ class TADSpec
   end
 
   def self.print_test_results(results)
-    puts results
+    results.each do |result|
+      puts result
+    end
   end
 
 end
